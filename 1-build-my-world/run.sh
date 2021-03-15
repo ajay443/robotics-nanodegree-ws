3bd5a2f3f098931ac65c6966cad222ed316c31f9
Ajay Ramesh <ajaycs14@gmail.com>
2021-03-14 20:41:17 -0700

    feat: build my world project

---
+++ 1-build-my-world/run.sh
@@ -0,0 +1,2 @@
+export GAZEBO_PLUGIN_PATH=$(pwd)/build
+gazebo world/my.world $1
---
+++ 1-build-my-world/script/hello.cpp
@@ -0,0 +1,53 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Welcome to Ajay Ramesh's World!\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
